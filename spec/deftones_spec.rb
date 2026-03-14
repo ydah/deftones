b6dcce204f7dcf47d66800eb55e96b349722e147
ydah <t.yudai92@gmail.com>
2026-03-14 23:43:11 +0900

    Initial commit

---
+++ spec/deftones_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Deftones do
+  it "has a version number" do
+    expect(Deftones::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
